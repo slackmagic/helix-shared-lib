c52b24d56b84c7376d7e2349dc33faf73078f6e9
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-11-07 19:29:50 +0100

    ENH - Add conf lib

---
+++ helix-config-lib/src/lib.rs
@@ -0,0 +1,63 @@
+#[macro_use]
+extern crate serde_derive;
+
+use crate::version::Version;
+use std::env;
+pub mod version;
+
+pub struct Configuration {
+    version: Version,
+}
+
+impl Configuration {
+    pub fn new(&self, version: Version) -> Self {
+        //Load configuration into env variables.
+        match env::args().len() {
+            1 => {
+                //_Defaut file loaded.
+                println!("[Configuration file] Default file loaded");
+                dotenv::dotenv().expect("File .env not found");
+            }
+            _ => {
+                //_Load first file params.
+                let arguments: Vec<String> = env::args().collect();
+                println!("[Configuration file] '{}' file loaded.", arguments[1]);
+                dotenv::from_filename(&arguments[1]).expect("File .env not found");
+            }
+        }
+
+        Configuration { version }
+    }
+
+    pub fn get_served_addr(&self) -> String {
+        let mut addr = String::new();
+        let ip = env::var("IP").expect("IP not found.");
+        let port = env::var("PORT").expect("PORT not found.");
+
+        //Return string of {IP}:{PORT}
+        addr.push_str(&ip);
+        addr.push_str(&":".to_owned());
+        addr.push_str(&port);
+        addr
+    }
+
+    pub fn get_workers_number(&self) -> usize {
+        let str_value = env::var("ACTIX_WORKERS").expect("ACTIX_WORKERS not found.");
+        str_value.parse::<usize>().unwrap()
+    }
+
+    pub fn get_shutdown_time_out(&self) -> u64 {
+        let str_value =
+            env::var("ACTIX_SHUTDOWN_TIMEOUT").expect("ACTIX_SHUTDOWN_TIMEOUT not found.");
+        str_value.parse::<u64>().unwrap()
+    }
+
+    pub fn get_keep_alive(&self) -> usize {
+        let str_value = env::var("ACTIX_KEEP_ALIVE").expect("ACTIX_KEEP_ALIVE not found.");
+        str_value.parse::<usize>().unwrap()
+    }
+
+    pub fn version(&self) -> &Version {
+        &self.version
+    }
+}
---
+++ helix-config-lib/src/version.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Version {
+    pub version: String,
+    pub git_version: GitVersion,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GitVersion {
+    pub commit_short_hash: String,
+    pub commit_message: String,
+    pub commit_date: String,
+}
+
+impl Version {
+    pub fn new(
+        version: String,
+        git_commit_short_hash: String,
+        git_commit_message: String,
+        git_commit_date: String,
+    ) -> Version {
+        Version {
+            version: version,
+            git_version: GitVersion {
+                commit_message: git_commit_message,
+                commit_short_hash: git_commit_short_hash,
+                commit_date: git_commit_date,
+            },
+        }
+    }
+}

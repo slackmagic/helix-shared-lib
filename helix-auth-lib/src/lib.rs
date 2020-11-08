9d8ac3af3a826746552f707f0a0febf9a44222aa
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-11-08 10:00:02 +0100

    ENH - Update Auth methods

--- helix-auth-lib/src/lib.rs
+++ helix-auth-lib/src/lib.rs
@@ -5,9 +5,10 @@ pub mod error;
 pub mod middleware;
 mod tokenizer;
 
+use crate::claims::Claims;
 use crate::error::*;
+use actix_web::HttpRequest;
 use chrono::prelude::*;
-use claims::Claims;
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 use std::env;
@@ -19,6 +20,50 @@ impl HelixAuth {
         match HelixAuth::get_token_data(token).is_ok() {
             true => Ok(()),
             false => Err(HelixAuthError::InvalidToken),
+        }
+    }
+
+    pub fn get_claimer(req: &HttpRequest) -> Option<Claims> {
+        match req.headers().get("Authorization") {
+            Some(value) => match HelixAuth::get_token_data(&value.to_str().unwrap().to_string()) {
+                Ok(data) => Some(data),
+                Err(_) => None,
+            },
+            None => None,
+        }
+    }
+
+    pub fn generate_keys(
+        user: &str,
+        user_uuid: &uuid::Uuid,
+        person_uuid: &uuid::Uuid,
+    ) -> Result<(String, String), String> {
+        let api_auth_key = env::var("API_AUTH_KEY").expect("API_AUTH_KEY not found.");
+
+        let tokenizer_access = tokenizer::Tokenizer::new(api_auth_key.to_string());
+        let result_access = tokenizer_access
+            .claims(claims::get_access_token_claims(
+                &user,
+                &user_uuid,
+                &person_uuid,
+            ))
+            .generate();
+
+        let tokenizer_refresh = tokenizer::Tokenizer::new(api_auth_key.to_string());
+        let result_refresh = tokenizer_refresh
+            .claims(claims::get_refresh_token_claims(
+                &user,
+                &user_uuid,
+                &person_uuid,
+            ))
+            .generate();
+
+        match result_access {
+            Ok(a) => match result_refresh {
+                Ok(r) => Ok((a, r)),
+                Err(_) => Err("Oops, an error occured.".to_owned()),
+            },
+            Err(_) => (Err("Oops, an error occured.".to_owned())),
         }
     }
 
@@ -66,39 +111,5 @@ impl HelixAuth {
         hasher.input_str(&to_hash);
 
         hasher.result_str()
-    }
-
-    fn generate_keys(
-        user: &str,
-        user_uuid: &uuid::Uuid,
-        person_uuid: &uuid::Uuid,
-    ) -> Result<(String, String), String> {
-        let api_auth_key = env::var("API_AUTH_KEY").expect("API_AUTH_KEY not found.");
-
-        let tokenizer_access = tokenizer::Tokenizer::new(api_auth_key.to_string());
-        let result_access = tokenizer_access
-            .claims(claims::get_access_token_claims(
-                &user,
-                &user_uuid,
-                &person_uuid,
-            ))
-            .generate();
-
-        let tokenizer_refresh = tokenizer::Tokenizer::new(api_auth_key.to_string());
-        let result_refresh = tokenizer_refresh
-            .claims(claims::get_refresh_token_claims(
-                &user,
-                &user_uuid,
-                &person_uuid,
-            ))
-            .generate();
-
-        match result_access {
-            Ok(a) => match result_refresh {
-                Ok(r) => Ok((a, r)),
-                Err(_) => Err("Oops, an error occured.".to_owned()),
-            },
-            Err(_) => (Err("Oops, an error occured.".to_owned())),
-        }
     }
 }

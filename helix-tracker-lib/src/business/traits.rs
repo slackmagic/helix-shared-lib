a70b204c203b534ff1112b48fe2bb9d563eee76b
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-11-14 10:45:26 +0100

    Update

--- helix-tracker-lib/src/business/traits.rs
+++ helix-tracker-lib/src/business/traits.rs
@@ -4,7 +4,9 @@ use crate::core::log::Log;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
-pub trait TrackerInteractorTrait<I: Serialize + DeserializeOwned, L: Serialize + DeserializeOwned> {
+pub trait TrackerInteractorTrait<I: Serialize + DeserializeOwned, L: Serialize + DeserializeOwned>:
+    Send
+{
     //fn create_item<T: DeserializeOwned>(&self, item: Item<T>) -> TrackerDomainResult<Item<T>>;
 
     fn get_items(

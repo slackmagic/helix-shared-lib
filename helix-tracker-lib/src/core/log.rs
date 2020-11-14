b0b9e5914dcaf3be2d40ca2047c1f8066d4577e5
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-11-14 19:01:15 +0100

    ENH: Temporary update on Hash

--- helix-tracker-lib/src/core/log.rs
+++ helix-tracker-lib/src/core/log.rs
@@ -3,7 +3,7 @@ use chrono::prelude::*;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Log<T> {
     pub uuid: uuid::Uuid,
-    pub hash: String,
+    pub hash: Option<String>,
     pub data: Option<T>,
     pub created_on: Option<DateTime<Utc>>,
     pub item_id: i32,
